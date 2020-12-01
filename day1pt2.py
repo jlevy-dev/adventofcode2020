d5c917e4cf5c25516342c5866a6507206d1f5c31
Justin Levy <jnlemail@jslnet.com>
2020-12-01 13:54:05 -0800

    Delete day1pt2.py

--- day1pt2.py
+++
@@ -1,9 +0,0 @@
-with open("input.txt") as f:
-    l = [int(x.strip()) for x in f]
-
-for i, n1 in enumerate(l):
-    for j, n2 in enumerate(l[i+1:]):
-        for k, n3 in enumerate(l[j+1:]):
-            if n1 + n2 + n3 == 2020:
-                print(n1 * n2 * n3)
-                
